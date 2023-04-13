9505aa33c0ebd73d1abfa44f51f957d815f91e77
sapiens12 <sapiens12@nate.com>
2023-04-14 01:29:02 +0200

    feat: main 수정 footer

--- components/Layouts/Footer.js
+++ components/Layouts/Footer.js
@@ -16,140 +16,55 @@ const Footer = () => {
                 data-aos-delay="200"
               >
                 <Link href="/" className="logo">
-                  <img src="/images/white-logo.png" alt="Image" />
+                  <img src="/images/footer-logo.png" alt="Image" />
                 </Link>
-
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Quaerat molestiae corporis, magni maxime perferendis ducimus
-                  totam officiis sit exercitationem sed odio debitis minus
-                  cumque dolores dicta.
-                </p>
-
-                <ul className="social-icon">
-                  <li>
-                    <a href="https://www.facebook.com/" target="_blank">
-                      <i className="bx bxl-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://twitter.com/" target="_blank">
-                      <i className="bx bxl-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.linkedin.com/" target="_blank">
-                      <i className="bx bxl-linkedin"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="https://www.pinterest.com/" target="_blank">
-                      <i className="bx bxl-pinterest-alt"></i>
-                    </a>
-                  </li>
-                </ul>
+                
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-6 col-md-6">
               <div
                 className="single-widget"
                 data-aos="fade-in"
                 data-aos-duration="1200"
                 data-aos-delay="300"
               >
-                <h3>Services</h3>
-                <ul>
+                <ul className="footer_menu">
                   <li>
-                    <Link href="/service-details">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Big Data
-                    </Link>
+                    <a>
+                      회사소개
+                    </a>
                   </li>
                   <li>
-                    <Link href="/service-details">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      UI/UX Design
-                    </Link>
+                    <a>
+                      멤버십소개 
+                    </a>
                   </li>
                   <li>
-                    <Link href="/service-details">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Desktop Application
-                    </Link>
+                    <a>
+                      미술품
+                    </a>
                   </li>
                   <li>
-                    <Link href="/service-details">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Mobile Application
-                    </Link>
+                    <a>
+                      포인트몰 
+                    </a>
                   </li>
                   <li>
-                    <Link href="/service-details">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Product Engineering
-                    </Link>
+                    <a>
+                      전시 및 이벤트 
+                    </a>
                   </li>
                   <li>
-                    <Link href="/service-details">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Machine Learning
-                    </Link>
+                    <a>
+                      고객지원
+                    </a>
                   </li>
                 </ul>
               </div>
             </div>
 
-            <div className="col-lg-3 col-md-6">
-              <div
-                className="single-widget"
-                data-aos="fade-in"
-                data-aos-duration="1200"
-                data-aos-delay="400"
-              >
-                <h3>Important Links</h3>
-
-                <ul>
-                  <li>
-                    <Link href="/about-1">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      About Us
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/services">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Services
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/pricing">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Pricing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/team">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Team
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/contact">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Support
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/testimonials">
-                      <i className="right-icon bx bx-chevrons-right"></i>
-                      Testimonials
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
+            
             <div className="col-lg-3 col-md-6">
               <div
                 className="single-widget"
@@ -157,27 +72,10 @@ const Footer = () => {
                 data-aos-duration="1200"
                 data-aos-delay="500"
               >
-                <h3>Information</h3>
 
-                <ul className="information">
-                  <li className="address">
-                    <i className="flaticon-call"></i>
-                    <span>Phone</span>
-                    +882-569-756
-                  </li>
-
-                  <li className="address">
-                    <i className="flaticon-envelope"></i>
-                    <span>Email</span>
-                    hello@jumpx.com
-                  </li>
-
-                  <li className="address">
-                    <i className="flaticon-maps-and-flags"></i>
-                    <span>Address</span>
-                    123, Western Road, Melbourne Australia
-                  </li>
-                </ul>
+                <Link href="/" className="read-more footer_btn">
+                  상담 신청 하기
+                </Link>
               </div>
             </div>
           </div>
@@ -193,33 +91,22 @@ const Footer = () => {
       <footer className="footer-bottom-area">
         <div className="container">
           <div className="row align-items-center">
-            <div className="col-lg-4">
+            <div className="col-lg-6">
               <div className="copy-right">
-                <p>Copyright &copy; {currentYear} Jumpx. All Rights Reserved</p>
+                <p>Copyright &copy; {currentYear} GREEN GALLERY. All Rights Reserved</p>
               </div>
             </div>
 
-            <div className="col-lg-5">
+            <div className="col-lg-6">
               <div className="privacy">
                 <ul>
                   <li>
-                    <Link href="/terms-conditions">Terms & Conditions</Link>
+                    <Link href="/terms-conditions">개인정보 처리방침</Link>
                   </li>
                   <li>
-                    <Link href="/privacy-policy">Privacy Policy</Link>
+                    <Link href="/privacy-policy">이용약관</Link>
                   </li>
                 </ul>
-              </div>
-            </div>
-
-            <div className="col-lg-3">
-              <div className="designed">
-                <p>
-                  Designed By <i className="bx bx-heart"></i>{" "}
-                  <a href="https://envytheme.com/" target="_blank">
-                    EnvyTheme
-                  </a>
-                </p>
               </div>
             </div>
           </div>
