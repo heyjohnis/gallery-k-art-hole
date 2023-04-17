82538a31ba503c27863022675cf09487c5728a22
sapiens12 <sapiens12@nate.com>
2023-04-17 13:38:58 +0200

    fix: 골프장 목록 테이블 수정

--- components/Benefits/TableGolf.js
+++ components/Benefits/TableGolf.js
@@ -8,13 +8,11 @@ const TableGolf = () => {
     <>
       <section className="business-area pt-70">
         <div className="container">
-          <div className="row align-items-center mb-30">
+          <div className="row align-items-center">
             <div className="col-lg-12">
               <div className="business-content text-center">
                 <span>Green Gallery Golf</span>
-                <h2>골프 서비스</h2>
-                <p>전국 300여 곳의 프리미엄 골프장을 활용할 수 있습니다.<br/>
-                    300억원 상당의 회원권 소지한 것 이상의 혜택을 받으실 수 있습니다.</p>
+                <h2>골프 제휴골프장 목록</h2>
               </div>
             </div>
           </div>
