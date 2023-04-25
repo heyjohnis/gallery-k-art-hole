79336b067b6f2c6fe44df60db0244c98ef177074
sapiens12 <sapiens12@nate.com>
2023-04-25 17:25:45 +0200

    fix: qna 수정

--- components/Faq/FaqContent.js
+++ components/Faq/FaqContent.js
@@ -28,17 +28,14 @@ const FaqContent = () => {
                   <AccordionItem uuid="a">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        질문 1
+                      그린갤러리 골프멤버십은 어떤건가요? 
                       </AccordionItemButton>
                     </AccordionItemHeading>
 
                     <AccordionItemPanel>
                       <p>
-                      인간이 보배를 무엇이 것이다.보라, 열매를 미인을 아름답고 눈이 사막이다. 
-                      설산에서 따뜻한 낙원을 소금이라 이상의 청춘은 인생을 들어 보라. 
-                      천하를 피어나는 끓는 어디 칼이다. 품에 이것은 안고, 설레는 것은 끓는다. 
-                      꾸며 이것을 그들에게 커다란 싹이 이것이야말로 천지는 쓸쓸하랴? 
-                      발휘하기 이 설레는 대한 듣는다. 인도하겠다는 우리의 청춘의 것이다.
+                      그린갤러리 골프멤버십은 골프와 예술이 만나 탄생한 새로운 문화의 골프멤버십입니다.<br/>
+                      멤버십 하나로 전국 300개의 골프장과 미술품, 부가 혜택까지 즐기실 수 있습니다.  
                       </p>
                     </AccordionItemPanel>
                   </AccordionItem>
@@ -46,17 +43,14 @@ const FaqContent = () => {
                   <AccordionItem uuid="b">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        질문 1
+                      그린갤러리 상품가입은 어떻게 하나요? 
                       </AccordionItemButton>
                     </AccordionItemHeading>
 
                     <AccordionItemPanel>
                       <p>
-                      인간이 보배를 무엇이 것이다.보라, 열매를 미인을 아름답고 눈이 사막이다. 
-                      설산에서 따뜻한 낙원을 소금이라 이상의 청춘은 인생을 들어 보라. 
-                      천하를 피어나는 끓는 어디 칼이다. 품에 이것은 안고, 설레는 것은 끓는다. 
-                      꾸며 이것을 그들에게 커다란 싹이 이것이야말로 천지는 쓸쓸하랴? 
-                      발휘하기 이 설레는 대한 듣는다. 인도하겠다는 우리의 청춘의 것이다.
+                      가입신청서 작성 후 가입을 도와드리고 있습니다.<br/>
+                      1588-1778로 문의주시면 가입에 대한 상담을 도와드리겠습니다. 
                       </p>
                     </AccordionItemPanel>
                   </AccordionItem>
@@ -64,16 +58,12 @@ const FaqContent = () => {
                   <AccordionItem uuid="c">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        질문 3
+                      멤버십 사전 가입 시 혜택이 있나요?
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <p>
-                      인간이 보배를 무엇이 것이다.보라, 열매를 미인을 아름답고 눈이 사막이다. 
-                      설산에서 따뜻한 낙원을 소금이라 이상의 청춘은 인생을 들어 보라. 
-                      천하를 피어나는 끓는 어디 칼이다. 품에 이것은 안고, 설레는 것은 끓는다. 
-                      꾸며 이것을 그들에게 커다란 싹이 이것이야말로 천지는 쓸쓸하랴? 
-                      발휘하기 이 설레는 대한 듣는다. 인도하겠다는 우리의 청춘의 것이다.
+                      멤버십 가입 시 유명 브랜드커스터마이징 골프백 등 차별화된 사은품을 증정합니다. 
                       </p>
                     </AccordionItemPanel>
                   </AccordionItem>
@@ -81,16 +71,12 @@ const FaqContent = () => {
                   <AccordionItem uuid="d">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        질문 4
+                      나의 등급 및 잔여 포인트는 어디서 확인 할 수 있나요? 
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <p>
-                      인간이 보배를 무엇이 것이다.보라, 열매를 미인을 아름답고 눈이 사막이다. 
-                      설산에서 따뜻한 낙원을 소금이라 이상의 청춘은 인생을 들어 보라. 
-                      천하를 피어나는 끓는 어디 칼이다. 품에 이것은 안고, 설레는 것은 끓는다. 
-                      꾸며 이것을 그들에게 커다란 싹이 이것이야말로 천지는 쓸쓸하랴? 
-                      발휘하기 이 설레는 대한 듣는다. 인도하겠다는 우리의 청춘의 것이다.
+                      나의 등급 확인은 그린갤러리 홈페이지 로그인 후 마이페이지에서 확인하실 수 있습니다.
                       </p>
                     </AccordionItemPanel>
                   </AccordionItem>
@@ -98,16 +84,13 @@ const FaqContent = () => {
                   <AccordionItem uuid="e">
                     <AccordionItemHeading>
                       <AccordionItemButton>
-                        질문 5
+                      미술품을 선택할 수 있나요? 
                       </AccordionItemButton>
                     </AccordionItemHeading>
                     <AccordionItemPanel>
                       <p>
-                      인간이 보배를 무엇이 것이다.보라, 열매를 미인을 아름답고 눈이 사막이다. 
-                      설산에서 따뜻한 낙원을 소금이라 이상의 청춘은 인생을 들어 보라. 
-                      천하를 피어나는 끓는 어디 칼이다. 품에 이것은 안고, 설레는 것은 끓는다. 
-                      꾸며 이것을 그들에게 커다란 싹이 이것이야말로 천지는 쓸쓸하랴? 
-                      발휘하기 이 설레는 대한 듣는다. 인도하겠다는 우리의 청춘의 것이다.
+                      고객의 선호도와 취향에 맞는 미술품을 구성해드립니다.<br/>
+                      원하시는 미술품을 선택하실 수 있습니다. 
                       </p>
                     </AccordionItemPanel>
                   </AccordionItem>
