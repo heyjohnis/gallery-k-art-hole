05ba57e09c1ebd228f5b6aca570ad9a068c4d5db
sapiens12 <sapiens12@nate.com>
2023-05-01 14:13:50 +0200

    feat: 제휴서비스 소개 페이지

--- components/Ggmall/Associate.js
+++ components/Ggmall/Associate.js
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import React from "react";
+import styles from "./Ggmall.module.scss";
 
 const Associate = () => {
   return (
@@ -8,140 +10,131 @@ const Associate = () => {
           <div className="row align-items-center">
             <div className="col-lg-12">
               <div className="business-content text-center">
-                <span>Green Gallery Benefits</span>
-                <h2>그린갤러리 부가혜택</h2>
-                <p>골프여행, 호텔, 리조트, 프라이빗 파티 등 다양한 부가 서비스를 제공합니다. </p>
-              </div>
-              <div className="business-content text-center mt-100">
-                <h2>자사 보유 컨텐츠</h2>
+                <h2>그린갤러리 제휴서비스</h2>
+                <p>그린갤러리와 함께 하는 다양한 제휴 혜택을 받아보세요.</p>
               </div>
             </div>
           </div>
 
-          <div className="row">
+          <div className="row mt-50">
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card1.png" alt="Image" />
+                  <img src="/images/ggmall/gg1.png" alt="가입 고객 특전 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>갤러리카페 & 큐레이팅</h3>
+                <div className={styles.items}>
+                  <h3>가입 고객 특전</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card2.png" alt="Image" />
+                  <img src="/images/ggmall/gg2.png" alt="리무진 서비스 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>여수, 제주, 부산 등 호텔</h3>
+                <div className={styles.items}>
+                  <h3>리무진 서비스(자사)</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card3.png" alt="Image" />
+                  <img src="/images/ggmall/gg3.png" alt="리무진 서비스 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>프리미엄 골프여행 (여수)</h3>
+                <div className={styles.items}>
+                  <h3>리무진 서비스(제휴)</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card4.png" alt="Image" />
+                  <img src="/images/ggmall/gg4.png" alt="여수 1박 2일 숙박 서비스 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>전국 주요 리조트, 부대시설</h3>
+                <div className={styles.items}>
+                  <h3>여수 1박 2일 숙박 서비스</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card5.png" alt="Image" />
+                  <img src="/images/ggmall/gg5.png" alt="여수 2박 3일 숙박 서비스 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>마리나 요트, 제트보트</h3>
+                <div className={styles.items}>
+                  <h3>여수 2박 3일 숙박 서비스</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card6.png" alt="Image" />
+                  <img src="/images/ggmall/gg6.png" alt="여수 요트 이용 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>승마, 로드FC 등</h3>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-
-          <div className="row align-items-center mt-100">
-            <div className="col-lg-12">
-              <div className="business-content text-center">
-                <h2>제휴 컨텐츠</h2>
-              </div>
-            </div>
-          </div>
-          
-          <div className="row">
-            <div className="col-lg-4 col-md-6">
-              <div className="single-news">
-                <div>
-                  <img src="/images/benefits/card7.png" alt="Image" />
-                </div>
-
-                <div className="news-content-wrap service-content-warp">
-                  <h3>골프연습장 (예정)</h3>
+                <div className={styles.items}>
+                  <h3>여수 요트 이용</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card8.png" alt="Image" />
+                  <img src="/images/ggmall/gg7.png" alt="프로 동반 라운드 서비스 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>굿즈 (예정)</h3>
+                <div className={styles.items}>
+                  <h3>프로 동반 라운드 서비스</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
             <div className="col-lg-4 col-md-6">
-              <div className="single-news">
+              <div className={styles.item_card}>
                 <div>
-                  <img src="/images/benefits/card9.png" alt="Image" />
+                  <img src="/images/ggmall/gg8.png" alt="제주 1박 2일 숙박 서비스 사진" />
                 </div>
 
-                <div className="news-content-wrap service-content-warp">
-                  <h3>국내외 일반/골프 여행패키지 (예정)</h3>
+                <div className={styles.items}>
+                  <h3>제주 1박 2일 숙박 서비스</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className={styles.item_card}>
+                <div>
+                  <img src="/images/ggmall/gg9.png" alt="제주 2박3일 숙박 서비스 사진" />
+                </div>
+
+                <div className={styles.items}>
+                  <h3>제주 2박3일 숙박 서비스</h3>
+                  <Link href="#" className="default-btn">서비스 신청</Link> 
                 </div>
               </div>
             </div>
 
           </div>
-
-
         </div>
       </section>
     </>
