5ea643b4efe41842b26461c0829a25b8b3c85fa2
김희준 <juni0227@nate.com>
2023-05-01 14:30:51 +0900

    feat: 멤버십에 블루, 레드, 퍼플 등급 콘텐츠 추가

--- components/Mypage/Content/Membership.js
+++ components/Mypage/Content/Membership.js
@@ -2,13 +2,19 @@ import React from 'react';
 import LvBlack from '../LvBlack';
 import LvPlatinum from "../LvPlatinum";
 import LvGreen from "../LvGreen";
+import LvPurple from '../LvPurple';
+import LvRed from '../LvRed';
+import LvBlue from '../LvGreen';
 
 const Membership = ({ user }) => {
     return (
         <>
-        { user.membership === 'black' && <LvBlack/> }
-        { user.membership === 'platinum' && <LvPlatinum/> }
-        { user.membership === 'green' && <LvGreen/> }
+            { user.membership === 'black' && <LvBlack/> }
+            { user.membership === 'platinum' && <LvPlatinum/> }
+            { user.membership === 'blue' && <LvBlue/> }
+            { user.membership === 'green' && <LvGreen/> }
+            { user.membership === 'purple' && <LvPurple/> }
+            { user.membership === 'red' && <LvRed/> }
         </>
     );
 };
