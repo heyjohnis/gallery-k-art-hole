07065ae2936b0584fcbe16f173cec05d92f039c6
heyjohnis <juni0227@nate.com>
2023-05-25 17:49:23 +0900

    fix: Pagination 개발 및 작품출력 개발

--- components/Artworks/ArtworkList.js
+++ components/Artworks/ArtworkList.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Pagination from "../Pagination";
+import baseUrl from "../../utils/baseUrl";
+import cookie from "js-cookie";
+
 import ArtworkThumbnail from "./ArtworkThumbnail";
 
 const ArtworkList = () => {
-  const artworks = [
-    { atwk_no: 1234, atwk_name: "무제", atwk_price: "100000", atwk_img: "" },
-    { atwk_no: 1235, atwk_name: "무제", atwk_price: "100000", atwk_img: "" },
-  ];
+  const [artworks, setArtworks] = useState([]);
+  const [page, setPage] = useState({ currentPage: 1, totalPages: 1 });
+
+  const getArtworkData = () => {
+    const url = `${baseUrl}/artworks`;
+    const medq_token = cookie.get("medq_token");
+    axios({
+      method: "post",
+      url: url,
+      headers: { Authorization: `Bearer ${medq_token}` },
+      data: page,
+    })
+      .then(({ data }) => {
+        console.log("data: ", data);
+        setArtworks(data.list);
+        setPage(data.pagination);
+      })
+      .finally(() => {});
+  };
+
+  const gotoPage = (pageNumber) => {
+    console.log("pageNumber: ", pageNumber);
+    setPage((prevState) => ({ ...prevState, currentPage: pageNumber }));
+  };
+
+  useEffect(() => {
+    getArtworkData();
+  }, [page.currentPage]);
+
   return (
     <section className="team-area ptb-100">
       <div className="container">
-        {artworks.map((artwork) => (
-          <ArtworkThumbnail key={artwork.atwk_no} artwork={artwork} />
-        ))}
+        <div className="row">
+          {artworks.map((artwork) => (
+            <ArtworkThumbnail key={artwork.atwk_no} artwork={artwork} />
+          ))}
+        </div>
       </div>
-      <Pagination />
+      <Pagination page={page} goto={gotoPage} />
     </section>
   );
 };
--- components/Pagination.js
+++ components/Pagination.js
@@ -1,31 +1,69 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 
-const Pagination = () => {
+const PaginationComp = ({ page, goto }) => {
+  const [currentPage, setCurrentPage] = useState(page.currentPage || 1);
+
+  let totalPage = page.totalPages;
+  let countDisplayPage = totalPage - 5 > 0 ? 5 : totalPage;
+  const middlePage = parseInt(countDisplayPage / 2);
+  let startPageNumber =
+    currentPage - middlePage > 0 ? currentPage - middlePage : 1;
+  startPageNumber =
+    totalPage - currentPage - middlePage > 0
+      ? startPageNumber
+      : startPageNumber + (totalPage - currentPage - middlePage);
+  console.log("currentPage: ", currentPage);
+
+  let items = [];
+  for (let i = 0; i < countDisplayPage; i++) {
+    const pageNumber = startPageNumber + i;
+
+    items.push(
+      <li
+        className={`page-item ${pageNumber === currentPage ? "active" : ""}`}
+        key={pageNumber}
+        onClick={() => gotoPage(pageNumber)}
+      >
+        <a className="page-link">{pageNumber}</a>
+      </li>
+    );
+  }
+
+  const gotoBeforePageSet = () => {
+    const pageNumber =
+      currentPage - countDisplayPage > 0 ? currentPage - countDisplayPage : 1;
+    setCurrentPage(pageNumber);
+    goto(pageNumber);
+  };
+
+  const gotoAfterPageSet = () => {
+    const pageNumber =
+      totalPage - (countDisplayPage + currentPage) > 0
+        ? countDisplayPage + currentPage
+        : totalPage;
+    setCurrentPage(pageNumber);
+    goto(pageNumber);
+  };
+
+  const gotoPage = (pageNumber) => {
+    goto(pageNumber);
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="col-lg-12">
       <div className="page-navigation-area">
         <nav aria-label="Page navigation example text-center">
           <ul className="pagination">
             <li className="page-item">
-              <a className="page-link page-links">
+              <a className="page-link page-links" onClick={gotoBeforePageSet}>
                 <i className="bx bx-chevrons-left"></i>
               </a>
             </li>
-
-            <li className="page-item active">
-              <a className="page-link">1</a>
-            </li>
-
+            {items}
             <li className="page-item">
-              <a className="page-link">2</a>
-            </li>
-
-            <li className="page-item">
-              <a className="page-link">3</a>
-            </li>
-
-            <li className="page-item">
-              <a className="page-link">
+              <a className="page-link" onClick={gotoAfterPageSet}>
                 <i className="bx bx-chevrons-right"></i>
               </a>
             </li>
@@ -36,4 +74,4 @@ const Pagination = () => {
   );
 };
 
-export default Pagination;
+export default PaginationComp;
