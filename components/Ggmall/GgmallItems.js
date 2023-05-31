5ee076f9988615c240353844358d585fc2fcb056
김희준 <juni0227@nate.com>
2023-06-01 03:50:27 +0900

    feat: option 합산까지 개발

--- components/Ggmall/GgmallItems.js
+++ components/Ggmall/GgmallItems.js
@@ -7,49 +7,54 @@ import { commaFormat } from "../../utils/number";
 
 const GgmallItems = ({ content, options }) => {
   const [optionComp, setOptionComp] = useState([]);
-  const [selectedVals, setSelectedVals] = useState({});
-  const [textSelectedOption, setTextSelectedOption] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [price, setPrice] = useState({});
 
-  const selectedOptionStr = () => {
-    let text = "";
-    Object.keys(selectedVals).forEach((key) => {
-      console.log(`${key}: ${selectedVals[key]} `);
-      text += `${key}: ${selectedVals[key]} `;
-    });
-    setTextSelectedOption(text);
-  };
+  const selectOption = (optionNo, option) => {
+    console.log("selected option: ", option);
+    setPrice((prev) => ({
+      ...prev,
+      [optionNo]: parseInt(option.price),
+    }));
 
-  const selectOption = (idx, optionName, e) => {
-    selectedVals[optionName] = e.target.value;
-    setSelectedVals({ ...selectedVals });
-    selectedOptionStr();
+    console.log("total", price);
   };
 
   const renderOptions = () => {
     const comp = [];
     for (let i = 0; i < options.length; i++) {
-      const optionValue = options[i].option_value;
-      const splitOptions = optionValue.split(",");
       const optionArr = [];
       optionArr.push(<option key={0}>선택</option>);
-      for (let j = 0; j < splitOptions.length; j++) {
+
+      const jsonArr = JSON.parse(options[i].option_value);
+      for (let j = 0; j < jsonArr.length; j++) {
         optionArr.push(
-          <option key={j + 1} value={splitOptions[j]}>
-            {splitOptions[j]}
+          <option key={j + 1} value={jsonArr[j].price}>
+            {jsonArr[j].op_name}{" "}
+            {parseInt(jsonArr[j].price)
+              ? `(+${commaFormat(jsonArr[j].price)}P)`
+              : ""}
           </option>
         );
       }
       comp.push(
-        <div className={styles.info_wrap}>
-          <span className={styles.tit}>{options[i].option}</span>
-          <select
-            key={i}
-            onChange={(e) => {
-              selectOption(i, options[i].option, e);
-            }}
-          >
-            {optionArr}
-          </select>
+        <div key={i} className="row">
+          <div className="col-md-12">
+            <label className={styles.tit}>{options[i].option_name}</label>
+            <select
+              key={i}
+              onChange={(e) => {
+                let data = jsonArr[e.target.selectedIndex - 1];
+                if (e.target.selectedIndex === 0) {
+                  data = { op_name: "", price: 0 };
+                }
+                console.log("data: ", e.target.selectedIndex - 1);
+                selectOption(options[i].option_no, data);
+              }}
+            >
+              {optionArr}
+            </select>
+          </div>
         </div>
       );
     }
@@ -58,7 +63,17 @@ const GgmallItems = ({ content, options }) => {
 
   useEffect(() => {
     renderOptions();
+    setPrice((prev) => ({ ...prev, price: parseInt(content.price) }));
   }, [options]);
+
+  useEffect(() => {
+    if (price) {
+      const total = Object.values(price).reduce(function add(sum, currValue) {
+        return sum + currValue;
+      }, 0);
+      setTotalPrice(total);
+    }
+  }, [price]);
 
   return (
     <>
@@ -73,14 +88,9 @@ const GgmallItems = ({ content, options }) => {
             <div className="col-lg-6 col-md-12">
               <div className={styles.product_details_desc}>
                 <h2>{content.pd_name}</h2>
-                {/* <div className={styles.info_wrap}>
-                  <span className={styles.tit}>브랜드:</span>
-                  {content.brand}
-                </div> */}
                 {optionComp}
-                {/* <div className={styles.info_wrap}><span className={styles.tit}>수량선택:</span></div> */}
                 <h2 className={styles.price}>
-                  {commaFormat(content.price || 0)}
+                  {commaFormat(totalPrice)}
                   <span>P</span>
                 </h2>
                 <div className={styles.btn_warp}>
@@ -89,7 +99,6 @@ const GgmallItems = ({ content, options }) => {
                       pathname: "/ggmall/payment/",
                       query: {
                         pd_no: content.pd_no,
-                        option: textSelectedOption,
                       },
                     }}
                     className={`default-btn ${styles.btn}`}
