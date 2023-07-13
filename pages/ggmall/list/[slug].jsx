487eabc2fcc06b3a0c56854d7dc25f2f12375951
heyjohnis <juni0227@nate.com>
2023-07-13 11:19:19 +0900

    feat GK-314: 사은품 페이지 상단에 이미지 추가 요청

--- pages/ggmall/list/[slug].jsx
+++ pages/ggmall/list/[slug].jsx
@@ -56,7 +56,15 @@ const ggList = ({ user }) => {
         homePageText={pdKind === "gift" ? "MEMBER PRIVILEGES" : "GG MALL"}
         activePageText={pdKindName}
       />
+      {pdKind === "gift" && (
+        <section className="team-area mt-5">
+          <div className="container">
+            <img src="/images/gift_guide.jpg" alt="gift_guide" />
+          </div>
+        </section>
+      )}
       <SearchKeyword searchWords={getContents} serviceKind={pdKind} />
+
       <GgmallList contents={contents} productKind={pdKind} />
 
       <Footer />
