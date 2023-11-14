4d9d8b64f217c9a74fead1149346323226caa1b1
나성재 <>
2023-11-14 13:27:41 +0900

    feat GK-1139: 그린갤러리-마이페이지-주문/배송조회 페이지 수정

--- components/Mypage/Content/OrderComp.js
+++ components/Mypage/Content/OrderComp.js
@@ -3,15 +3,33 @@ import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import styles from "../Mypage.module.scss";
 import { commaFormat, hyphenForPhone } from "../../../utils/number";
+import baseUrl from "../../../utils/baseUrl";
+import axios from "axios";
 
 const OrderComp = ({ order }) => {
   console.log("order: ", order);
+  console.log("baseUrl: ", baseUrl);
   const [isShow, setIsShow] = useState(false);
   const [toggle, setToggle] = useState("toggle_on");
 
   const showDetail = () => {
     setIsShow(!isShow);
     setToggle(!isShow ? "toggle_off" : "toggle_on");
+  };
+
+  const cancelOrder = (order_no) => {
+    console.log("cancelOrder: ", order_no);
+    const url = `${baseUrl}/mall/order/cancel`;
+    console.log("url: ", url);
+    axios({ method: "post", url, data: { order_no } })
+      .then(({ data }) => {
+        console.log("data: ", data);
+        alert("주문이 취소되었습니다.");
+        location.reload();
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
   };
 
   return (
@@ -24,16 +42,30 @@ const OrderComp = ({ order }) => {
             </td>
             <td width="*">
               {order.order_date}
-              <br />[{order.order_status}] {order.pd_name}
+              {/* [{order.order_status}]  */}
+              <br />{order.pd_name}
               <br />
               {commaFormat(order.total_price || 0)} P
             </td>
             <td width="50px">
               <div
+                style={{ textAlign: 'center' }}
                 className={`${styles.btn_detail} ${toggle}`}
                 onClick={showDetail}
               >
                 상세보기
+              </div>
+              <div style={{ display: 'flex', marginTop: '20px' }}>
+                [{order.order_status}]
+                {["01", "09"].includes(order.order_status_code) && (
+                  <div
+                    style={{ marginLeft: '10px' }}
+                    className={`default-btn ${styles.btn} ${styles.btn_conform}`}
+                    onClick={() => cancelOrder(order.order_no)}
+                  >
+                    신청취소
+                  </div>
+                )}
               </div>
             </td>
           </tr>
