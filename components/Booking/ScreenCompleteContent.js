be171b9fc54d0cc5c5e0a727aa4fd197c810d091
sapiens12 <sapiens12@nate.com>
2023-12-22 23:50:33 +0100

    feat: GG-14 예약 정보 컨텐츠 작성 완료

--- components/Booking/ScreenCompleteContent.js
+++ components/Booking/ScreenCompleteContent.js
@@ -1,47 +1,67 @@
 import React from 'react'
-import { CompleteContents } from '../Common/CompleteContents';
 import Table from "react-bootstrap/Table";
 
 export const ScreenCompleteContent = () => {
-  const complete = {
-    title : '제주 스위트룸 이용권',
-    date : '2023. 08. 14',
-    status: '신청완료',
-  }
-	
-  const person = [
-		["희망날짜", "2023. 08. 04"], 
-		["인원수", "3명"],
-		["주문자명", "홍길동"],
-    ["연락처", "010 -1234 - 5678"],
-    ["이메일", "greengallery1234@galleryk.co.kr"],
-    ["기타사항", "기타 스크린골프 포인트 서비스 문의사항을 작성해 주시기 바랍니다."]
-	]
-
-  const tableList = person.map((i) => (<CompleteContents item={i[0]} info={i[1]}/>));
 
 	return (
-    <div class="complete_container">
-    <Table>
-      <tbody>
-        <tr>
-          <td colSpan={2} className="complete_content_tit">{complete.title}</td>
-        </tr>
-        <tr>
-          <td>{complete.date}</td>
-          <td>{complete.status}</td>
-        </tr>
-      </tbody>
-      <tbody> 
-        {tableList}
-      </tbody>
-		</Table>
+    <>
+    <div className='complete_application_container'>
+      <span>신청번호 : <span>1000</span></span>
+      <span className='bar'>|</span>
+      <span>신청일자 : <span>2023. 08 .10</span></span>
+    </div>
+    <div class="complete_tbl_container">
+      <Table className='tbl_complete_content'>
+        <tbody className='complete_content_tit'>
+          <tr>
+            <td colSpan={2}><h3>스크린골프 포인트 서비스</h3></td>
+          </tr>
+          <tr>
+            <td className='tit_point'>300,000 P</td>
+            <td className='tit_status'><span>신청완료</span></td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="content_hr_bottom"></td>
+          </tr>
+        </tbody>
+        <tbody className='complete_content_items'> 
+          <tr>
+            <td colSpan={2} className='content_padding'></td>
+          </tr>
+          <tr>
+            <td>희망날짜</td>
+            <td className='items_detail'><span>2023. 08. 04</span></td>
+          </tr>
+          <tr>
+            <td>인원수</td>
+            <td className='items_detail'><span>3명</span></td>
+          </tr>
+          <tr>
+            <td>주문자명</td>
+            <td className='items_detail'><span>홍길동</span></td>
+          </tr>
+          <tr>
+            <td>연락처</td>
+            <td className='items_detail'><span>010 - 1234 - 5678</span></td>
+          </tr>
+          <tr>
+            <td>이메일</td>
+            <td className='items_detail'><span>greengallery1234@galleryk.co.kr</span></td>
+          </tr>
+          <tr>
+            <td>기타사항</td>
+            <td className='items_detail'>기타 스크린골프 포인트 서비스 문의사항을 작성해 주시기 바랍니다.</td>
+          </tr>
+        </tbody>
+      </Table>
+
+    </div>
     <button
       type="submit"
-      className="default-btn btn-two"
+      className="complete_btn"
     >
       예약현황
     </button>
-    </div>
+  </>
 	);
 }
