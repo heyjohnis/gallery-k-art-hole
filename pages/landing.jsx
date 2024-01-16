7dada5e24f64afe0b0ce4cb40d7ee6e088498f61
galleryk <resource@galleryk.co.kr>
2024-01-16 11:42:16 +0900

    fix: 랜딩페이지 이미지 업데이트 남장현

--- pages/landing.jsx
+++ pages/landing.jsx
@@ -28,9 +28,9 @@ useEffect(() => {
   const handWindowSize = () => {
     const { innerWidth } = window;
     if (innerWidth < 768) {
-      setChangeImg("/images/landing_240115_m_v2.jpg");
+      setChangeImg("/images/landing_240116_m.jpg");
     } else {
-      setChangeImg("/images/landing_240115_v2.jpg");
+      setChangeImg("/images/landing_240116.jpg");
     }
   };
   
