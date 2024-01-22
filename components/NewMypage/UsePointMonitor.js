32c94c0c45539472e3de596df1dc276087ccd361
김희준 <juni0227@nate.com>
2024-01-22 10:15:14 +0900

    feat: 등급별로 사용가능 횟수 확인

--- components/NewMypage/UsePointMonitor.js
+++ components/NewMypage/UsePointMonitor.js
@@ -5,7 +5,7 @@ export default function UsePointMonitor({ user }) {
   const isFirstHalf = now.getMonth() < 6;
   return (
     <>
-      {!user.quartly_count && (
+      {user?.monthly_count > 0 && (
         <>
           <li>
             <span className="list_tit">
@@ -32,42 +32,50 @@ export default function UsePointMonitor({ user }) {
           </li>
         </>
       )}
-      <li>
-        <span className="list_tit">
-          {Math.ceil(new Date().getMonth() + 1 / 3)}분기 혜택 횟수
-        </span>
-        <span className="list_item">
-          {user.monthly_count * 3 - (user.use_qaurterly_count || 0)}회 /{" "}
-          {user.monthly_count * 3}회
-        </span>
-      </li>
-      <li>
-        <span className="list_tit">
-          {Math.ceil(new Date().getMonth() + 4 / 3)}분기 혜택 횟수
-        </span>
-        <span className="list_item">
-          {user.monthly_count * 3 - (user.use_next_qaurterly_count || 0)} 회 /{" "}
-          {user.monthly_count * 3}회
-        </span>
-      </li>
-      <li>
-        <span className="list_tit">
-          {isFirstHalf ? "상" : "하"}반기 혜택 횟수
-        </span>
-        <span className="list_item">
-          {user.monthly_count * 6 - (user.use_qaurterly_count || 0)} 회 /{" "}
-          {user.monthly_count * 6}회
-        </span>
-      </li>
-      <li>
-        <span className="list_tit">
-          {!isFirstHalf ? "상" : "하"}반기 혜택 횟수
-        </span>
-        <span className="list_item">
-          {user.monthly_count * 6 - (user.use_next_qaurterly_count || 0)} 회 /{" "}
-          {user.monthly_count * 6}회
-        </span>
-      </li>
+      {user?.quarterly_count > 0 && (
+        <>
+          <li>
+            <span className="list_tit">
+              {Math.ceil(new Date().getMonth() + 1 / 3)}분기 혜택 횟수
+            </span>
+            <span className="list_item">
+              {user.quarterly_count - (user.use_qaurterly_count || 0)}회 /{" "}
+              {user.quarterly_count}회
+            </span>
+          </li>
+          <li>
+            <span className="list_tit">
+              {Math.ceil(new Date().getMonth() + 4 / 3)}분기 혜택 횟수
+            </span>
+            <span className="list_item">
+              {user.quarterly_count - (user.use_next_qaurterly_count || 0)} 회 /{" "}
+              {user.quarterly_count}회
+            </span>
+          </li>
+        </>
+      )}
+      {user?.half_yearly_count > 0 && (
+        <>
+          <li>
+            <span className="list_tit">
+              {isFirstHalf ? "상" : "하"}반기 혜택 횟수
+            </span>
+            <span className="list_item">
+              {user.half_yearly_count - (user.use_qaurterly_count || 0)} 회 /{" "}
+              {user.half_yearly_count}회
+            </span>
+          </li>
+          <li>
+            <span className="list_tit">
+              {!isFirstHalf ? "상" : "하"}반기 혜택 횟수
+            </span>
+            <span className="list_item">
+              {user.half_yearly_count - (user.use_next_qaurterly_count || 0)} 회
+              / {user.half_yearly_count}회
+            </span>
+          </li>
+        </>
+      )}
       <li>
         <span className="list_tit">연간 혜택 횟수</span>
         <span className="list_item">
