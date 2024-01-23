9037e79db46782de1f61ac8bdc51ee1dcf658bdd
sapiens12 <sapiens12@nate.com>
2024-01-23 16:42:04 +0100

    feat: 개발 오류 요청사항 59 포인트 이용내역 기능 추가

--- components/NewMypage/MyPoint.js
+++ components/NewMypage/MyPoint.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { POST } from "../../hooks/restApi";
 import Pagination from "../Pagination";
+import Form from "react-bootstrap/Form";
+import { NoContent } from './NoContent';
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 
 export default function MyPoint({ user }) {
   const [form, setForm] = useState({});
@@ -33,7 +37,7 @@ export default function MyPoint({ user }) {
   return (
     <div className="mypoint_container">
       <h1>포인트 이용내역</h1>
-      <article className="complete_title mypoint_tit">
+      {/* <article className="complete_title mypoint_tit">
         <img src="/images/mypage/ico_mypoint.png" />
         <h2 className="mypoint_headline">사용 가능 포인트</h2>
         <p className="mypoint_point">
@@ -42,12 +46,24 @@ export default function MyPoint({ user }) {
           ).toLocaleString()}{" "}
           P
         </p>
-      </article>
+      </article> */}
       <section className="mypoint_content">
         <section className="mypoint_info">
-          <h3>회원님의 이용현황</h3>
           <ul className="mypoint_info_list">
+            {/* TODO: 내용바뀜 */}
             <li>
+              <span className="tit">지급 포인트</span>
+              <span className="item">1,000,000,000 P</span>
+            </li>
+            <li>
+              <span className="tit">총 사용 포인트</span>
+              <span className="item">- 7,500,000 P</span>
+            </li>
+            <li>
+              <span className="tit">잔여 포인트</span>
+              <span className="item">992,500,0000 P</span>
+            </li>
+            {/* <li>
               <span className="tit">총 연간 포인트</span>
               <span className="item">
                 {parseInt(user?.yearly_point || 0).toLocaleString()} P
@@ -84,11 +100,61 @@ export default function MyPoint({ user }) {
             <li>
               <span className="tit">종료일</span>
               <span className="item">{user.end_date}</span>
-            </li>
+            </li> */}
           </ul>
         </section>
+        <section className='mypoint_filter'>
+          <section className="mb_reser_filter">
+            <ToggleButtonGroup
+              type="radio"
+              name="term"
+              defaultValue=""
+            >
+              <ToggleButton id="period-radio-1" value="" className="btn_filter">
+                전체
+              </ToggleButton>
+              <ToggleButton id="period-radio-2" value="30" className="btn_filter">
+                1개월
+              </ToggleButton>
+              <ToggleButton id="period-radio-3" value="90" className="btn_filter">
+                3개월
+              </ToggleButton>
+              <ToggleButton id="period-radio-3" value="180" className="btn_filter">
+                6개월
+              </ToggleButton>
+              <ToggleButton
+                id="period-radio-4"
+                value="365"
+                className="btn_filter"
+              >
+                1년
+              </ToggleButton>
+              <ToggleButton
+                id="period-radio-5"
+                value="1095"
+                className="btn_filter"
+              >
+                3년
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </section>
+          <div className='period'>기준일자 : 2024-01-17 ~ 2024-02-16</div>
+          <Form.Select
+            aria-label="사용기간"
+          >
+            <option value="">사용기간</option>
+            <option value="">전체</option>
+            <option value="30">1개월</option>
+            <option value="90">3개월</option>
+            <option value="365">6개월</option>
+            <option value="1095">1년</option>
+            <option value="1095">3년</option>
+          </Form.Select>
+        </section>
+        {/* TODO: 필터 추가됨 */}
+        <NoContent/>
+        {/* TODO: 이용내역이 없을 경우 */}
         <section className="mypoint_history">
-          <h3>포인트 이용내역</h3>
           <ul className="mypoint_history_list">
             {points.map((item, i) => (
               <li key={i}>
