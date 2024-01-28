4ce978588b1de8b6123a821f98ca09da1c4e7725
김희준 <juni0227@nate.com>
2024-01-28 10:14:43 +0900

    feat: multi-range 샘플적용

--- components/GgShopping/GgShoppingFilter.js
+++ components/GgShopping/GgShoppingFilter.js
@@ -1,94 +1,99 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
+import React from "react";
+import { Form } from "react-bootstrap";
+import MultiRangeSlider from "../Common/MultiRangeSilder";
 
 export default function GgShoppingFilter() {
-
   return (
-    <div className='shopping_filter'>
-      <h2 className='total'>전체</h2>
+    <div className="shopping_filter">
+      <h2 className="total">전체</h2>
       <section>
         <h2>브랜드</h2>
-        <div className='filter_group'>
-            <Form.Check
-              label="EISEN HEIM"
-              name="brand1"
-              type="checkbox"
-              id="check_1"
-              className="item"
-            />
-            <Form.Check
-              label="BOGNER"
-              name="brand2"
-              type="checkbox"
-              id="check_2"
-              className="item"
-            />
-            <Form.Check
-              label="PING"
-              name="brand3"
-              type="checkbox"
-              id="check_3"
-              className="item"
-            />
-            <Form.Check
-              label="YAMAHA"
-              name="brand4"
-              type="checkbox"
-              id="check_4"
-              className="item"
-            />
-            <Form.Check
-              label="Phi Golf"
-              name="brand5"
-              type="checkbox"
-              id="check_5"
-              className="item"
-            />
+        <div className="filter_group">
+          <Form.Check
+            label="EISEN HEIM"
+            name="brand1"
+            type="checkbox"
+            id="check_1"
+            className="item"
+          />
+          <Form.Check
+            label="BOGNER"
+            name="brand2"
+            type="checkbox"
+            id="check_2"
+            className="item"
+          />
+          <Form.Check
+            label="PING"
+            name="brand3"
+            type="checkbox"
+            id="check_3"
+            className="item"
+          />
+          <Form.Check
+            label="YAMAHA"
+            name="brand4"
+            type="checkbox"
+            id="check_4"
+            className="item"
+          />
+          <Form.Check
+            label="Phi Golf"
+            name="brand5"
+            type="checkbox"
+            id="check_5"
+            className="item"
+          />
         </div>
       </section>
       <section>
         <h2>제품</h2>
-        <div className='filter_group'>
-            <Form.Check
-              label="골프채"
-              name="product1"
-              type="checkbox"
-              id="pro_1"
-              className="item"
-            />
-            <Form.Check
-              label="캐디백"
-              name="product2"
-              type="checkbox"
-              id="pro_2"
-              className="item"
-            />
-            <Form.Check
-              label="보스턴백"
-              name="product3"
-              type="checkbox"
-              id="pro_3"
-              className="item"
-            />
-            <Form.Check
-              label="골프웨어"
-              name="product4"
-              type="checkbox"
-              id="pro_4"
-              className="item"
-            />
-            <Form.Check
-              label="스윙연습기"
-              name="product5"
-              type="checkbox"
-              id="pro_5"
-              className="item"
-            />
+        <div className="filter_group">
+          <Form.Check
+            label="골프채"
+            name="product1"
+            type="checkbox"
+            id="pro_1"
+            className="item"
+          />
+          <Form.Check
+            label="캐디백"
+            name="product2"
+            type="checkbox"
+            id="pro_2"
+            className="item"
+          />
+          <Form.Check
+            label="보스턴백"
+            name="product3"
+            type="checkbox"
+            id="pro_3"
+            className="item"
+          />
+          <Form.Check
+            label="골프웨어"
+            name="product4"
+            type="checkbox"
+            id="pro_4"
+            className="item"
+          />
+          <Form.Check
+            label="스윙연습기"
+            name="product5"
+            type="checkbox"
+            id="pro_5"
+            className="item"
+          />
         </div>
-      </section>     
+      </section>
       <section>
         <h2>가격</h2>
-        <div className='filter_group'>
+        <MultiRangeSlider
+          min={10000}
+          max={2000000}
+          onChange={({ min, max }) => console.log(`min = ${min}, max = ${max}`)}
+        />
+        <div className="filter_group">
           <Form.Check
             label="10,000,000 ~"
             name="price1"
@@ -111,8 +116,7 @@ export default function GgShoppingFilter() {
             className="item"
           />
         </div>
-      </section>    
+      </section>
     </div>
-  )
+  );
 }
-
