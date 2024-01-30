8c2b541993f502b025688415257773e5cc163fc0
sapiens12 <sapiens12@nate.com>
2024-01-30 08:09:51 +0100

    fix: GG-31 에러페이지 수정

--- pages/ggshopping/error.jsx
+++ pages/ggshopping/error.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { CompleteTitle } from "../../components/Common/CompleteTitle";
+import PageBanner from "../../components/Common/PageBanner";
+import Footer from "../../components/Layouts/Footer";
 import RecommendService from "../../components/RecommendService";
 
 export default function error() {
   return (
     <div>
+      <PageBanner />
       <CompleteTitle
         iconUrl={`/images/ggshopping/ico_error_tit.png`}
         title="제품을 "
@@ -13,6 +16,7 @@ export default function error() {
       />
       <button className="btn_white mt-100">상품 담으러 가기</button>
       {/* <RecommendService /> */}
+      <Footer />
     </div>
   );
 }
