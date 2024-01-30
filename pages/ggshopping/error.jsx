ab1d4b0f8d1314688d06734fcbabca27ec841d14
sapiens12 <sapiens12@nate.com>
2024-01-30 06:30:50 +0100

    feat: GG-31 에러페이지 PC

---
+++ pages/ggshopping/error.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { CompleteTitle } from "../../components/Common/CompleteTitle";
+import RecommendService from "../../components/RecommendService";
+
+export default function error() {
+  return (
+    <div>
+      <CompleteTitle
+        iconUrl={`/images/ggshopping/ico_error_tit.png`}
+        title="제품을 "
+        title2="담아주세요"
+        text="그린갤러리의 다양한 제휴서비스와 엄선된 상품을 만나보세요."
+      />
+      <button className="btn_white mt-100">상품 담으러 가기</button>
+      {/* <RecommendService /> */}
+    </div>
+  );
+}
